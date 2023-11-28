3f21326af5c7edfe3e7668e3d937a70e924c5d86
Maylla Nascimento <101286317+maylla04@users.noreply.github.com>
2023-11-28 17:11:07 -0300

    Update routes.js

--- routers/routes.js
+++ routers/routes.js
@@ -4,6 +4,7 @@ const routes = express.Router();
 const userController = require('../controllers/userController');
 
 routes.get('/users', userController.findUsers);
+routes.get('/users/:id', userController.findUser);
 routes.post('/user', userController.createUser);
 routes.delete('/user/:id', userController.deleteUser);
 routes.put('/user/:id', userController.updateUser);
